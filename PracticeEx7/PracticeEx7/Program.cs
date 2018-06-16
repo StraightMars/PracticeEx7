3794fe86d57874ef6e4edd591967cb7eb118828b
nureevmarsel <mnurev@mail.ru>
2018-06-16 14:43:53 +0500

    Arrays

--- PracticeEx7/PracticeEx7/Program.cs
+++ PracticeEx7/PracticeEx7/Program.cs
@@ -8,6 +8,33 @@ namespace PracticeEx7
 {
     class Program
     {
+        static double[] ArraySort(double[] arr)
+        {
+            Array.Sort(arr);
+            return arr;
+        }
+        static void CreatingArrays(double[] arr, double[] temp)
+        {
+            Array.Resize(ref temp, temp.Length - 1);
+            temp[0] = arr[0] + arr[1];
+            for (int i = 1; i < temp.Length; i++)
+            {
+                temp[i] = arr[i + 1];
+            }
+            ShowArr(temp);
+        }
+        static void ShowArr(double[] arr)
+        {
+            Console.WriteLine("Указанные частоты: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("{0}: {1}", i + 1, arr[i]);
+            }
+        }
+        static void CreateTree(double[] arr)
+        {
+
+        }
         static double ScanDouble()
         {
             bool ok;
@@ -30,22 +57,23 @@ namespace PracticeEx7
             bool flag = false;
             bool ok;
             int N;
+            Console.WriteLine("Введите количество букв в алфавите: ");
             do
             {
-                Console.WriteLine("Введите количество букв в алфавите: ");
-                do
+                ok = Int32.TryParse(Console.ReadLine(), out N);
+                if (!ok)
+                    Console.WriteLine("Ошибка! Введите натуральное число!");
+                if (N <= 0)
                 {
-                    ok = Int32.TryParse(Console.ReadLine(), out N);
-                    if (!ok)
-                        Console.WriteLine("Ошибка! Введите натуральное число!");
-                    if (N <= 0)
-                    {
-                        Console.WriteLine("Ошибка! Введите натуральное число!");
-                        ok = false;
-                    }
-                } while (!ok);
+                    Console.WriteLine("Ошибка! Введите натуральное число!");
+                    ok = false;
+                }
+            } while (!ok);
+            double[] arr = new double[N];
+            double[] temp = new double[N];
+            do
+            {
                 Console.WriteLine("Введите частоты букв по порядку, через Enter: ");
-                double[] arr = new double[N];
                 double summ = 0;
                 for (int i = 0; i < N; i++)
                 {
@@ -54,14 +82,18 @@ namespace PracticeEx7
                     summ += arr[i];
                 }
                 if (summ == 1)
+                {
                     flag = true;
+                    
+                }
                 else
                 {
                     Console.WriteLine("Сумма всех частот должна равняться единице! Повторите ввод с самого начала.");
                     flag = false;
                 }
             } while (!flag);
-
+            ArraySort(arr);
+            ShowArr(arr);
         }
     }
 }
